bd962e0784bcde83c73dc6724fadd22eaf9425e6
James Gibson <marblemunkey@gmail.com>
2013-05-18 12:15:20 -0400

    change to outline grid

--- badluckmaze.js
+++ badluckmaze.js
@@ -13,23 +13,26 @@ for (var i = 0; i < window.maze.BOARDSIZE; i++)
 {
 	for (var j = 0; j < window.maze.BOARDSIZE; j++)
 	{
-		if (fill == '000000')
-		{
-			fill = 'FFFFFF';
-		}
-		else
-		{
-			fill = '000000';
-		}
+		// if (fill == '000000')
+		// {
+			// fill = 'FFFFFF';
+		// }
+		// else
+		// {
+			// fill = '000000';
+		// }
 		var square = new createjs.Shape();
-		square.graphics.beginFill(fill).moveTo(window.maze.SQWIDTH/2+1,0);
+		square.graphics.beginStroke(fill).moveTo(window.maze.SQWIDTH/2+1,0);
 		square.graphics.lineTo(window.maze.SQWIDTH+1,window.maze.SQHEIGHT/2+1);
 		square.graphics.lineTo(window.maze.SQWIDTH/2+1,window.maze.SQHEIGHT+1);
-		square.graphics.lineTo(0,window.maze.SQHEIGHT/2+1).endFill();
+		square.graphics.lineTo(0,window.maze.SQHEIGHT/2+1);
+		square.graphics.lineTo(window.maze.SQWIDTH/2+1,0).endStroke();
 		
 		square.x = 825 + (window.maze.SQWIDTH/2+1) * i - (window.maze.SQWIDTH/2+1) * (j + 1);
 		square.y = (window.maze.SQHEIGHT/2+1) * i + (window.maze.SQHEIGHT/2+1) * j;
-
+		
+		square.name = i + 'x' + j;
+		
 		stage.addChild(square);
 	}
 	stage.update();
