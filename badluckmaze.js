cfd974cba1f591b0c00f796b3eb2bb102d85ab5d
James Gibson <marblemunkey@gmail.com>
2013-05-18 11:42:27 -0400

    generated board overlay

--- badluckmaze.js
+++ badluckmaze.js
@@ -1,10 +1,37 @@
-window.bl = new Object();
-window.bl.canvas = document.getElementById('myCanvas');
-window.bl.ctx = window.bl.canvas.getContext('2d');
-	var ctx = window.bl.ctx;
-	ctx.moveTo(10,5);
-	ctx.lineTo(90,5);
-	ctx.lineTo(100,50);
-	ctx.lineTo(0,50);
-	ctx.lineTo(10,5);
-	ctx.stroke();
+window.maze = new Object();
+window.maze.SQHEIGHT = 74;
+window.maze.SQWIDTH = 126;
+window.maze.BOARDSIZE = 13;
+	
+
+function init() {
+window.maze.stage = new createjs.Stage("myCanvas");
+var stage = window.maze.stage;
+var fill = '000000';
+
+for (var i = 0; i < window.maze.BOARDSIZE; i++)
+{
+	for (var j = 0; j < window.maze.BOARDSIZE; j++)
+	{
+		if (fill == '000000')
+		{
+			fill = 'FFFFFF';
+		}
+		else
+		{
+			fill = '000000';
+		}
+		var square = new createjs.Shape();
+		square.graphics.beginFill(fill).moveTo(window.maze.SQWIDTH/2+1,0);
+		square.graphics.lineTo(window.maze.SQWIDTH+1,window.maze.SQHEIGHT/2+1);
+		square.graphics.lineTo(window.maze.SQWIDTH/2+1,window.maze.SQHEIGHT+1);
+		square.graphics.lineTo(0,window.maze.SQHEIGHT/2+1).endFill();
+		
+		square.x = 800 + (window.maze.SQWIDTH/2+1) * i - (window.maze.SQWIDTH/2+1) * (j + 1);
+		square.y = (window.maze.SQHEIGHT/2+1) * i + (window.maze.SQHEIGHT/2+1) * j;
+
+		stage.addChild(square);
+	}
+	stage.update();
+}
+}
