5fd85dc87f12981ce39f4b75234952d219813ffd
James Gibson <marblemunkey@gmail.com>
2013-05-18 12:31:45 -0400

    add board array

--- badluckmaze.js
+++ badluckmaze.js
@@ -2,6 +2,11 @@ window.maze = new Object();
 window.maze.SQHEIGHT = 74;
 window.maze.SQWIDTH = 126;
 window.maze.BOARDSIZE = 13;
+window.maze.board = new Array();
+for (var i = 0; i < 13; i++)
+{
+	window.maze.board[i] = new Array();
+}
 	
 
 function init() {
@@ -13,14 +18,6 @@ for (var i = 0; i < window.maze.BOARDSIZE; i++)
 {
 	for (var j = 0; j < window.maze.BOARDSIZE; j++)
 	{
-		// if (fill == '000000')
-		// {
-			// fill = 'FFFFFF';
-		// }
-		// else
-		// {
-			// fill = '000000';
-		// }
 		var square = new createjs.Shape();
 		square.graphics.beginStroke(fill).moveTo(window.maze.SQWIDTH/2+1,0);
 		square.graphics.lineTo(window.maze.SQWIDTH+1,window.maze.SQHEIGHT/2+1);
@@ -31,7 +28,8 @@ for (var i = 0; i < window.maze.BOARDSIZE; i++)
 		square.x = 825 + (window.maze.SQWIDTH/2+1) * i - (window.maze.SQWIDTH/2+1) * (j + 1);
 		square.y = (window.maze.SQHEIGHT/2+1) * i + (window.maze.SQHEIGHT/2+1) * j;
 		
-		square.name = i + 'x' + j;
+		square.name = 'grid' + i + 'x' + j;
+		window.maze.board[i][j] = square;
 		
 		stage.addChild(square);
 	}
